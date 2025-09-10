ddaeff0a24faebd50bbe5e79e88bfe3ab97850ad
Bin <12486952+bzhouzb@users.noreply.github.com>
2025-09-10 16:53:53 +0100

    Create package
    Added two functions
    - check_extraction
    - save_extraction

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# ncdrisc 0.0.1
+
+* Created the package
+* Added the `check_extraction()` function
+* Added the `save_extraction()` function
---
+++ data-raw/format-data.R
@@ -0,0 +1,13 @@
+
+library(devtools)
+
+# Load necessary data and save as part of package
+# This script needs to be run whenever the input files are updated
+
+# Standard country names
+countrylist <- read.csv("data-raw/country-list-2023-new.csv")
+use_data(countrylist)
+
+# standard variable name list - the list should be updated separately
+std_names_list <- read.csv("data-raw/standard_variable_names.csv")
+use_data(std_names_list)
